eae37c20f1d796d9f27e45641c1aaa9f29632082
Yu <beneditosahd@gmail.com>
2026-03-18 14:50:39 +0100

    chore: remove generated output folders and package-lock from tracking

--- output_sqlite/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductsController;
-use App\Http\Controllers\OrdersController;
-
-Route::apiResource('/products', ProductsController::class);
-Route::apiResource('/orders', OrdersController::class);
